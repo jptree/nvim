c37faeb32c2d0616210814a0ea2c18a84f986ce0
Petri, Jason F <jasonfpetri@outlook.com>
2022-12-01 19:15:37 -0500

    initial commit

---
+++ lua/petri/init.lua
@@ -0,0 +1,2 @@
+require("petri.set")
+require("petri.packer")
---
+++ lua/petri/packer.lua
@@ -0,0 +1,7 @@
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'folke/tokyonight.nvim'
+    use 'tpope/vim-commentary'
+    use 'davidhalter/jedi-vim'
+    use 'jmcantrell/vim-virtualenv'
+end)
